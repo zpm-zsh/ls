2a6695f547491211da6f34e2a5da8d88f5e6538f
Grigorii Horos <horosgrisa@gmail.com>
2018-12-31 22:21:03 +0200

    Show hyperlinks

--- ls.plugin.zsh
+++ ls.plugin.zsh
@@ -6,42 +6,40 @@ if ! =ls --version >/dev/null 2>&1 ; then
   return -1
 fi
 
-
-_LS=(ls)
+_LS=(=ls)
 
 if (( $+commands[gls] )); then
-  _LS=(gls)
+  _LS=(=gls)
 fi
 
-_LS=($_LS -hF --group-directories-first --color --time-style=+%Y-%m-%d\ %H:%M)
+_LS=($_LS -hF --hyperlink --group-directories-first --time-style=+%Y-%m-%d\ %H:%M)
 
+function _is_ls_colored(){
+  if [[ "$CLICOLOR" = 1 ]]; then
+    echo "--color"
+  fi
+}
 
 if (( $+commands[grc] )); then
   _GRC=("grc" "--config=${${(%):-%x}:a:h}/conf.ls" )
 fi
 
-
 function ls(){
-  $_LS -ClxB  $@
+  $_LS $(_is_ls_colored) -C $@
 }
 compdef ls=ls
 
 function l(){
-  $_LS -ClxB  $@
+  $_LS $(_is_ls_colored) -C $@
 }
 compdef l=ls
 
-function lsd(){
-  $_LS -l -d $@ *(-/DN)
-}
-compdef lsd=ls
-
 function la(){
-  $_LS  -ClxB  -A $@
+  $_LS $(_is_ls_colored)  -C -A $@
 }
 compdef la=ls
 
 function ll(){
-  $_GRC $_LS -l  $@
+  $_GRC $_LS $(_is_ls_colored) -l $@
 }
 compdef ll=ls
