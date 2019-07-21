a493e75f0e207952563fc1b76efb294fe15577a5
Grigorii Horos <horosgrisa@gmail.com>
2019-07-21 19:41:01 +0300

    Add `exa` support

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
 # ls
 Zsh plugin for ls. It improves the output of `ls`, and adds the following aliases:
 
-* `l` - similar to `ls`
-* `la` - similar to `ls`, but show all files
-* `lsd` - show only directories
+* `l` - show files
+* `ls` - show files
+* `la` - show all files
 * `ll` - show files line by line
 
+![screenshot](./ls.png)
 
-![screenshot](./screenshot.png)
+This plugin supports [exa](https://github.com/ogham/exa), if installed then:
+
+* `l` - show files, without git ignored
+* `ls` - show files
+* `la` - show all files
+* `ll` - show files line by line
+
+![screenshot](./exa.png)
 
 #### Note
 For a better view, use a theme for `dircolors`, for example [dircolors-material](https://github.com/zpm-zsh/dircolors-material)
--- ls.plugin.zsh
+++ ls.plugin.zsh
@@ -1,54 +1,85 @@
 #!/usr/bin/env zsh
 
-
-if ! =ls --version >/dev/null 2>&1 ; then
-  echo This plugin doesn\'t support BSD ls, please install GNU ls
-  return -1
-fi
-
-_LS=(=ls)
-
-if (( $+commands[gls] )); then
-  _LS=(=gls)
-fi
-
-if $_LS --hyperlink >/dev/null 2>&1 ; then
-  _HYPERLINK='--hyperlink'
-fi
-
-_LS=($_LS -hF  --group-directories-first --time-style=+%Y-%m-%d\ %H:%M --quoting-style=literal)
-
-function _is_ls_colored(){
-  if [[ "$CLICOLOR" = 1 ]]; then
-    echo "--color $_HYPERLINK"
-  fi
-}
-
-if (( $+commands[grc] )); then
-  _GRC=("grc" "--config=${${(%):-%x}:a:h}/conf.ls" )
-fi
-
-function ls(){
-  $_LS $(_is_ls_colored) -C $@
-}
-compdef ls=ls
-
-function l(){
-  $_LS $(_is_ls_colored) -C $@
-}
-compdef l=ls
-
-function la(){
-  $_LS $(_is_ls_colored)  -C -A $@
-}
-compdef la=ls
-
-function ll(){
+if command -v exa >/dev/null; then
+  # Use exa
+  exa_params=('--git' '--icons' '--classify' '--group-directories-first' '--time-style=long-iso')
   
-  if [[ "$CLICOLOR" = 1 ]]; then
-    $_GRC  $_LS $(_is_ls_colored) -l $@
-  else
-    $_LS -l $@
+  function ls(){
+    exa $exa_params $@
+  }
+  compdef ls=exa
+  
+  function l(){
+    exa '--git-ignore' $exa_params $@
+  }
+  compdef l=exa
+  
+  function la(){
+    exa $exa_params  $@
+  }
+  compdef la=exa
+  
+  function ll(){
+    
+    exa --header --long $exa_params $@
+  }
+  compdef ll=exa
+  
+else
+  
+  if ! =ls --version >/dev/null 2>&1 ; then
+    echo This plugin doesn\'t support BSD ls, please install GNU ls
+    return -1
   fi
-}
-compdef ll=ls
+  
+  _ls=(=ls)
+  
+  if (( $+commands[gls] )); then
+    _ls=(=gls)
+  fi
+  
+  if $_ls --hyperlink >/dev/null 2>&1 ; then
+    _hyperlink='--hyperlink'
+  fi
+  
+  _ls_params=('-hF' '--group-directories-first' '--time-style=+%Y-%m-%d %H:%M' '--quoting-style=literal')
+  
+  function _is_ls_colored(){
+    if [[ "$CLICOLOR" = 1 ]]; then
+      echo "--color $_hyperlink"
+    fi
+  }
+  
+  if command -v grc >/dev/null; then
+    _grc=("grc" "--config=${${(%):-%x}:a:h}/conf.ls" )
+  fi
+  
+  function ls(){
+    $_ls $(_is_ls_colored) -C $@
+  }
+  compdef ls=ls
+  
+  function l(){
+    $_ls $(_is_ls_colored) -C $@
+  }
+  compdef l=ls
+  
+  function la(){
+    $_ls $(_is_ls_colored)  -C -A $@
+  }
+  compdef la=ls
+  
+  function ll(){
+    
+    if [[ "$CLICOLOR" = 1 ]]; then
+      $_grc  $_ls $(_is_ls_colored) -l $@
+    else
+      $_ls -l $@
+    fi
+  }
+  compdef ll=ls
+  
+  
+  
+fi
+
