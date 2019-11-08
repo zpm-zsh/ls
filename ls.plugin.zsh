021fb1e9f539e1bc73e4772b99b86dccb7d2c93a
Grigorii Horos <horosgrisa@gmail.com>
2019-11-08 22:46:44 +0200

    Fix

--- ls.plugin.zsh
+++ ls.plugin.zsh
@@ -10,23 +10,23 @@ if (( $+commands[exa] )); then
   exa_params=('--git' '--icons' '--classify' '--group-directories-first' '--time-style=long-iso' '--group' '--color-scale')
   
   function ls(){
-    exa "${exa_params[@]}" $@
+    exa "${exa_params}" $@
   }
   compdef ls=exa
   
   function l(){
-    exa --git-ignore "${exa_params[@]}" $@
+    exa --git-ignore "${exa_params}" $@
   }
   compdef l=exa
   
   function la(){
-    exa -a "${exa_params[@]}" $@
+    exa -a "${exa_params}" $@
   }
   compdef la=exa
   
   function ll(){
     
-    exa --header --long "${exa_params[@]}" $@
+    exa --header --long "${exa_params}" $@
   }
   compdef ll=exa
 else
@@ -42,35 +42,32 @@ else
   
   _ls_params=('-hF' '--group-directories-first' '--time-style=+%Y-%m-%d %H:%M' '--quoting-style=literal')
   
-  function _is_ls_colored(){
-    if [[ "$CLICOLOR" = 1 ]]; then
-      echo "--color $_hyperlink"
-    fi
-  }
+  if [[ "$CLICOLOR" != "0" ]]; then
+    _ls_params+=("--color" "${_hyperlink}")
+  fi
   
   if (( $+commands[grc] )); then
     _grc=("grc" "--config=${_DIRNAME}/conf.ls" )
   fi
   
   function ls(){
-    $_ls $(_is_ls_colored) ${_ls_params[@]} -C $@
+    $_ls ${_ls_params} -C $@
   }
   compdef ls=ls
   
   function l(){
-    $_ls $(_is_ls_colored) ${_ls_params[@]} -C $@
+    $_ls ${_ls_params} -C $@
   }
   compdef l=ls
   
   function la(){
-    $_ls $(_is_ls_colored) ${_ls_params[@]}  -C -A $@
+    $_ls ${_ls_params} -C -A $@
   }
   compdef la=ls
   
   function ll(){
-    
     if [[ "$CLICOLOR" = 1 ]]; then
-      $_grc  $_ls $(_is_ls_colored) ${_ls_params[@]} -l $@
+      $_grc $_ls ${_ls_params} -l $@
     else
       $_ls -l $@
     fi
