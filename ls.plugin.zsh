734dfc8e094192b37434a51c5f05b6b8588446ff
Grigorii Horos <horosgrisa@gmail.com>
2024-04-24 14:36:01 +0300

    Change

--- ls.plugin.zsh
+++ ls.plugin.zsh
@@ -98,7 +98,7 @@ else
     _ls_params+=('--hyperlink')
   fi
 
-  if [[ "$CLICOLOR" != "0" ]]; then
+  if [[ "$CLICOLOR" != "0" && -z "$NO_COLOR" ]]; then
     _ls_params+=('--color')
   fi
 
@@ -131,7 +131,6 @@ else
   compdef ll=ls
 
   function lt() {
-    find . -print | sed -e 's;[^/]*/;|;g;s;|; |;g'
+    tree $@
   }
 fi
-
